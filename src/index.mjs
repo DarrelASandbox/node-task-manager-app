0b6b0ed00b2ab72f5b13f56164c14c92be9ccf25
DarrelA <darrelaiscoding@gmail.com>
2022-02-26 17:15:44 +0800

    Integrating async/ await.

--- src/index.mjs
+++ src/index.mjs
@@ -10,61 +10,72 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // https://httpstatuses.com/
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => res.status(201).send(user))
-    .catch((error) => res.status(400).send(error));
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get('/users', (req, res) =>
-  User.find({})
-    .then((users) => res.send(users))
-    .catch((error) => res.status(500).send())
-);
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
 // The findById method will throw an error if the id you pass it is improperly formatted so you should see a 500 error most of the time.
 // However, if you pass in an id that is validly formatted, but does not exist in the database then you will get the 404 sent back.
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   if (!mongoose.isValidObjectId(req.params.id))
     return res.status(404).send('Invalid id.');
 
-  User.findById(req.params.id)
-    .then((user) => {
-      if (!user) return res.status(404).send('No user found.');
-      res.send(user);
-    })
-    .catch((error) => res.status(500).send(error));
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send('No user found.');
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then(() => res.status(201).send(task))
-    .catch((error) => res.status(400).send(error));
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get('/tasks/', (req, res) =>
-  Task.find()
-    .then((task) => {
-      if (task.length === 0) return res.status(404).send('No task found.');
-      res.send(task);
-    })
-    .catch((error) => res.status(500).send(error))
-);
+app.get('/tasks/', async (req, res) => {
+  try {
+    const task = await Task.find();
+    if (task.length === 0) return res.status(404).send('No task found.');
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(error);
+  }
+});
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   if (!mongoose.isValidObjectId(req.params.id))
     return res.status(404).send('Invalid id.');
 
-  Task.findById(req.params.id)
-    .then((task) => {
-      if (!task) return res.status(404).send('No task found.');
-      res.send(task);
-    })
-    .catch((error) => res.status(500).send(error));
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).send('No task found.');
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 app.listen(port, () => console.log(`Server on port ${port}`));
