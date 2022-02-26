2ebf7871c30f8c38c55dc228479cf53df8ce1f06
DarrelA <darrelaiscoding@gmail.com>
2022-02-26 12:29:00 +0800

    mongoose.mjs only serve to connect to db and model for tasks has been created.

--- src/db/mongoose.mjs
+++ src/db/mongoose.mjs
@@ -1,9 +1,2 @@
 import mongoose from 'mongoose';
-import validator from 'validator';
-
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
-
-const Task = mongoose.model('Task', {
-  description: { type: String, trim: true, required: true },
-  completed: { type: Boolean, default: false },
-});
--- src/index.mjs
+++ src/index.mjs
@@ -1,18 +1,28 @@
 import express from 'express';
 import './db/mongoose.mjs';
 import User from './models/users.mjs';
+import Task from './models/tasks.mjs';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// https://httpstatuses.com/
 app.post('/users', (req, res) => {
   const user = new User(req.body);
   user
     .save()
-    .then(() => res.send(user))
-    .catch((error) => console.log(error));
+    .then(() => res.status(201).send(user))
+    .catch((error) => res.status(400).send(error));
+});
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => res.status(201).send(task))
+    .catch((error) => res.status(400).send(error));
 });
 
 app.listen(port, () => console.log(`Server on port ${port}`));
