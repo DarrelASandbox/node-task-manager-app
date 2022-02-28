efe8f7de82621f32de1472e657946eb28d5d005a
DarrelA <darrelaiscoding@gmail.com>
2022-02-28 14:43:52 +0800

    Refactor code so that user can only read, update and delete their own account.

--- src/routers/usersRouter.mjs
+++ src/routers/usersRouter.mjs
@@ -1,7 +1,6 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import User from '../models/users.mjs';
 import auth from '../middleware/auth.mjs';
+import User from '../models/users.mjs';
 
 const usersRouter = new express.Router();
 
@@ -53,74 +52,32 @@ usersRouter.post('/users/logoutall', auth, async (req, res) => {
   }
 });
 
-usersRouter.get('/users/me', auth, async (req, res) => {
-  res.send(req.user);
+usersRouter.get('/users/me', auth, async (req, res) => res.send(req.user));
 
-  // try {
-  //   const users = await User.find();
-  //   if (users.length === 0) return res.status(404).send('No user found.');
-  //   res.send(users);
-  // } catch (e) {
-  //   res.status(500).send();
-  // }
-});
-
-// The findById method will throw an error if the id you pass it is improperly formatted so you should see a 500 error most of the time.
-// However, if you pass in an id that is validly formatted, but does not exist in the database then you will get the 404 sent back.
-usersRouter.get('/users/:id', async (req, res) => {
-  if (!mongoose.isValidObjectId(req.params.id))
-    return res.status(404).send('Invalid id.');
-
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).send('No user found.');
-    res.send(user);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-usersRouter.patch('/users/:id', async (req, res) => {
+usersRouter.patch('/users/me', auth, async (req, res) => {
   const updateFields = Object.keys(req.body);
   // Feature improvement: Dynamically fetching fields from the Mongoose model
   const allowedUpdateFields = ['name', 'age', 'email', 'password'];
   const isValidUpdateField = updateFields.every((updateField) =>
     allowedUpdateFields.includes(updateField)
   );
-
   if (!isValidUpdateField) return res.status(400).send('Invalid update field.');
 
-  if (!mongoose.isValidObjectId(req.params.id))
-    return res.status(404).send('Invalid id.');
-
   try {
-    const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).send('No user found.');
     updateFields.forEach(
-      (updateField) => (user[updateField] = req.body[updateField])
+      (updateField) => (req.user[updateField] = req.body[updateField])
     );
-    await user.save();
-
-    // Replace the code below with the code above to enable middleware.
-    // findByIdAndUpdate() bypasses mongoose which perform a direct operation on the database.
-
-    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    //   new: true,
-    //   runValidators: true,
-    // });
-    // https://mongoosejs.com/docs/validation.html#update-validators
-
-    res.send(user);
+    await req.user.save();
+    res.send(req.user);
   } catch (e) {
-    res.status(400).send(e);
+    res.status(400).send();
   }
 });
 
-usersRouter.delete('/users/:id', async (req, res) => {
+usersRouter.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) return res.status(404).send('No user found.');
-    res.send(user);
+    await User.deleteOne({ _id: req.user._id });
+    res.send(req.user);
   } catch (e) {
     res.status(500).send();
   }
