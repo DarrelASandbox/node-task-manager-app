670a99ef87a82f319bd8b1c940ba76cccd9c50ee
DarrelA <darrelaiscoding@gmail.com>
2022-03-01 10:28:31 +0800

    Validate file upload.

--- src/routers/usersRouter.mjs
+++ src/routers/usersRouter.mjs
@@ -1,10 +1,25 @@
 import express from 'express';
+import multer from 'multer';
 import auth from '../middleware/auth.mjs';
 import User from '../models/users.mjs';
-import multer from 'multer';
 
+// With callbacks, you typically want to pass the error as an argument instead of throwing an error.
+// That lets you respond to the error by adding code inside the callback.
+// If you throw an error, you'll see that the callback doesn't run as the callback is never called.
+// Instead, you'd need to add a try/catch statement to catch the error and respond to the error.
 const usersRouter = new express.Router();
-const upload = multer({ dest: 'avatar' });
+const upload = multer({
+  dest: 'avatar',
+  limits: { fileSize: 1_000_000 },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/))
+      return cb(
+        new Error('Please upload file only with jpg, jpeg or png format.')
+      );
+
+    cb(undefined, true);
+  },
+});
 
 // https://httpstatuses.com/
 usersRouter.post('/users', async (req, res) => {
@@ -78,7 +93,8 @@ usersRouter.patch('/users/me', auth, async (req, res) => {
 
 usersRouter.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
   try {
-    res.send();
+    if (!req.file) return res.send('Please attach file.');
+    res.send('Successfully uploaded.');
   } catch (e) {
     res.status(500).send();
   }
