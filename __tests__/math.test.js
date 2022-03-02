2d3363b94300945dbee5c79a97f6b782c3d1acb6
DarrelA <darrelaiscoding@gmail.com>
2022-03-02 12:51:08 +0800

    Testing Asynchronous Code.

--- __tests__/math.test.js
+++ __tests__/math.test.js
@@ -1,10 +1,45 @@
-import calculateTip from '../src/math.mjs';
-test('Should calculate total + tip?', () => {
+import { add, calculateTip } from '../src/math.mjs';
+
+test('Should calculate total + tip', () => {
   const total = calculateTip(10, 30);
   expect(total).toBe(13);
 });
 
-test('Should calculate total + default tip?', () => {
+test('Should calculate total + default tip', () => {
   const total = calculateTip(10);
   expect(total).toBe(12);
+});
+
+// If the expect statement fails, it throws an error and done() is not called.
+// If we want to see in the test log why it failed, we have to wrap expect in a try block and pass the error in the catch block to done.
+// Otherwise, we end up with an opaque timeout error that doesn't show what value was received by expect(data).
+test('async code test', (done) => {
+  setTimeout(() => {
+    try {
+      expect(1).toBe(1);
+      done();
+    } catch (e) {
+      done(e);
+    }
+  }, 2000);
+});
+
+test('Should add 2 numbers, promises', (done) => {
+  try {
+    add(2, 3).then((sum) => {
+      expect(sum).toBe(5);
+      done();
+    });
+  } catch (e) {
+    done(e);
+  }
+});
+
+test('Should add 2 numbers, async/ await', async () => {
+  try {
+    const sum = await add(2, 3);
+    expect(sum).toBe(5);
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
 });
--- src/math.mjs
+++ src/math.mjs
@@ -1,4 +1,14 @@
 const calculateTip = (total, tipInPercentage = 20) =>
   (total += total * (tipInPercentage / 100));
 
-export default calculateTip;
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) return reject('Numbers must be positive.');
+
+      resolve(a + b);
+    }, 250);
+  });
+};
+
+export { calculateTip, add };
